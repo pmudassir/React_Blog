81a33b4e7bce4c39ef8d7d1875585ae1d6ec6e46
pmudassir <pmuddasir@gmail.com>
2023-01-27 21:21:48 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from "../img/Logo.png"
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="logo"/>
+      <span>
+        Pen and Papper
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='posts'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
